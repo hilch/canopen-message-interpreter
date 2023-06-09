c20166568d3e81bcc028400cf72bc1c56bd1cefe
Christoph Hilchenbach <11504129+hilch@users.noreply.github.com>
2023-06-09 09:49:27 +0200

    Refresh samples

---
+++ samples/analyze.bat
@@ -0,0 +1,13 @@
+set SCRIPT=..\sources\analyze.py
+set FILE1="pcan1"
+set FILE2="pcan2"
+
+
+py %SCRIPT% -s %FILE1%.trc -o %FILE1%.csv -f pcan
+py %SCRIPT% -s %FILE2%.trc -o %FILE2%.csv -f pcan
+pause
+
+
+
+
+
